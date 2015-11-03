b857b884a8337449995becd3dbac7cc2498ea36c
Erwan VASSEURE <erwan.vasseure@canal-plus.com>
2015-11-03 16:45:11 +0100

    yapf -i cprint.py

--- cprint/cprint.py
+++ cprint/cprint.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 """
     Usage:
         >>> from cprint import *
@@ -11,16 +10,17 @@
 from __future__ import print_function
 import sys
 
+
 class cprint(object):
-    
+
     colors = {
-        'NONE' : '\033[0m',
-        'OK' : '\033[94m',
-        'INFO' : '\033[92m',
-        'WARNING' : '\033[93m',
-        'ERR' : '\033[91m',
-        'FATAL' : '\033[31m',
-        'ENDC' : '\033[0m'
+        'NONE': '\033[0m',
+        'OK': '\033[94m',
+        'INFO': '\033[92m',
+        'WARNING': '\033[93m',
+        'ERR': '\033[91m',
+        'FATAL': '\033[31m',
+        'ENDC': '\033[0m'
     }
 
     def __init__(self, str):
@@ -35,21 +35,24 @@ class cprint(object):
         """
             Prints in blue to stdout
         """
-        print(cprint.colors['OK'] + str + cprint.colors['ENDC'], file=sys.stdout)
+        print(cprint.colors['OK'] + str + cprint.colors['ENDC'],
+              file=sys.stdout)
 
     @staticmethod
     def info(str):
         """
             Prints in green to stdout
         """
-        print(cprint.colors['INFO'] + str + cprint.colors['ENDC'], file=sys.stdout)
+        print(cprint.colors['INFO'] + str + cprint.colors['ENDC'],
+              file=sys.stdout)
 
     @staticmethod
     def warn(str):
         """
             Prints in yellow to strerr
         """
-        print(cprint.colors['WARNING'] + str + cprint.colors['ENDC'], file=sys.stderr)
+        print(cprint.colors['WARNING'] + str + cprint.colors['ENDC'],
+              file=sys.stderr)
 
     @staticmethod
     def err(str, interrupt=False):
@@ -57,9 +60,12 @@ class cprint(object):
             Prints in brown to stderr
             interrupt=True: stops the program
         """
-        print(cprint.colors['ERR'] + str + cprint.colors['ENDC'], file=sys.stderr)
+        print(cprint.colors['ERR'] + str + cprint.colors['ENDC'],
+              file=sys.stderr)
         if interrupt:
-            print(cprint.colors['ERR'] + "Error: Program stopped." + cprint.colors['ENDC'], file=sys.stderr)
+            print(cprint.colors['ERR'] + "Error: Program stopped." +
+                  cprint.colors['ENDC'],
+                  file=sys.stderr)
             exit(1)
 
     @staticmethod
@@ -68,7 +74,10 @@ class cprint(object):
             Prints in red to stderr
             interrupt=True: stops the program
         """
-        print(cprint.colors['FATAL'] + str + cprint.colors['ENDC'], file=sys.stderr)
+        print(cprint.colors['FATAL'] + str + cprint.colors['ENDC'],
+              file=sys.stderr)
         if interrupt:
-            print(cprint.colors['FATAL'] + "Fatal error: Program stopped." + cprint.colors['ENDC'], file=sys.stderr)
+            print(cprint.colors['FATAL'] + "Fatal error: Program stopped." +
+                  cprint.colors['ENDC'],
+                  file=sys.stderr)
             exit(1)
